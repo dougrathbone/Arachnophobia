23dafc654e772c0f1896f95a5d21e0724327114e
Doug Rathbone <dougrathbone@gmail.com>
2011-10-22 18:54:01 +1100

    Initial commit

---
+++ Arachnophobia.IIS/RobotsTxt.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+
+namespace Arachnophobia.IIS
+{
+	/// <summary>
+	/// Arachnophobia Robot Header Module
+	/// Adds a 
+	/// </summary>
+	public class RobotsTxtHandler : IHttpHandler
+	{
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.Write("User-agent: *\nDisallow: /");
+			context.Response.End();
+		}
+	}
+}
