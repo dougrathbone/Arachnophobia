01a3dcb784e1d3ba1f84ac53ed58ece1f1a29d38
Doug Rathbone <dougrathbone@gmail.com>
2011-10-23 01:02:54 +1100

    Altered namespaces on Projects (housekeeping)

--- Arachnophobia.IIS/Properties/AssemblyInfo.cs
+++ Arachnophobia.IIS/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GlobalTxt.Handler")]
+[assembly: AssemblyTitle("Arachnophobia.IIS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("GlobalTxt.Handler")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Doug Rathbone")]
+[assembly: AssemblyProduct("Arachnophobia.IIS")]
+[assembly: AssemblyCopyright("Copyright © Doug Rathbone")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Arachnophobia.TestWebsite/Properties/AssemblyInfo.cs
+++ Arachnophobia.TestWebsite/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GlobalTxt.TestWebsite")]
+[assembly: AssemblyTitle("Arachnophobia.TestWebsite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("GlobalTxt.TestWebsite")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Doug Rathbone")]
+[assembly: AssemblyProduct("Arachnophobia.TestWebsite")]
+[assembly: AssemblyCopyright("Copyright © Doug Rathbone")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
